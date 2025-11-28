22c162f14b0875a724ded9e6d855a095cfb5f4cc
Md-Redwan <redwanhosen4@gmail.com>
2025-11-29 02:33:32 +0600

    Play Button added

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import HeroImg from "../assets/HeroImg.png";
 import { IoHome } from "react-icons/io5";
+import { IoIosPlay } from "react-icons/io";
 
 const Hero = () => {
   return (
-    <div className="bg-[#F2F6F7]">
-      <div className="container flex justify-between pt-23.5">
+    <div className="bg-[#F2F6F7] mb-52">
+      <div className="container flex justify-between pt-23.5 pb-43">
         <div>
           <p className="font-nunito flex gap-1 items-center mt-34.5">
             <IoHome className="text-[#FF5A3C]" />
@@ -17,16 +18,23 @@ const Hero = () => {
           <p className="font-nunito text-[#5C727D]">
             Lorem ipsum dolor sit amet, consectetur adipisicing
           </p>
-          <div className="mt-6.5">
-            <button className="py-3 px-6 bg-[#FF5A3C] text-white cursor-pointer">
+          <div className="mt-6.5 flex gap-6">
+            <div>
+              <button className="py-3 px-6 bg-[#FF5A3C] text-white cursor-pointer">
               View Properties
             </button>
-
+            </div>
+            <div className="py-2.5 px-2.5 bg-[#FFFFFF] rounded-full  cursor-pointer text-[#FF5A3C] text-[30px] shadow-box">
+              <IoIosPlay />
+            </div>
           </div>
         </div>
         <div>
           <img src={HeroImg} alt="" />
         </div>
+      </div>
+      <div className="container m-auto h-33 bg-[#e9aeae]">
+
       </div>
     </div>
   );
